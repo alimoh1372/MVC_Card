81dbe452b86925d5576dde65b8bce7ff8fa41428
Ali From NocEmam <alimohammadzade7976@gmail.com>
2023-01-19 07:04:52 +0330

    1401/10/29 07:04 complete the component and starting Componentview _project.cshtml

---
+++ Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Card.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public  string Image { get; set; }
+        public string Text { get; set; }
+    }
+
+}
---
+++ Models/Project.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Card.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public string Customer { get; set; }
+
+        public Project(int id, string name, string image, string description, string customer)
+        {
+            Id = id;
+            Name = name;
+            Image = image;
+            Description = description;
+            Customer = customer;
+        }
+    }
+
+
+ 
+  
+}
